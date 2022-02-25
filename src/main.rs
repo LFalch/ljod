ff4bc0c591ef9a92cebf44f38067c996fd210161
LFalch <lucas@wasd.dk>
2022-02-26 00:22:31 +0100

    Generalise stress

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use ljod::word::{Word, PRIM, SEC, NS, Syllable};
+use ljod::word::{Word, PRIM, SEC, NS, NumberedTone, Syllable};
 
 fn main() {
     let word = Word(vec![
         Syllable::from(PRIM, "s", "ʉuː", "ɹ"),
         Syllable::from(SEC, "", "eː", "p"),
         Syllable::from(NS, "l", "ɪ", "ɹ"),
+    ]);
+
+    println!("/{word}/");
+
+    let word = Word(vec![
+        Syllable::from(NumberedTone::new(2), "k", "oː", ""),
+        Syllable::from(NumberedTone::new(0), "n", "a", ""),
     ]);
 
     println!("/{word:#}/");
--- src/word.rs
+++ src/word.rs
@@ -11,32 +11,74 @@ pub enum Stress {
     None
 }
 
+impl Display for Stress {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Stress::None if f.sign_plus() => write!(f, "."),
+            Stress::None => Ok(()),
+            Stress::PrimaryStress => write!(f, "ˈ"),
+            Stress::SecondaryStress => write!(f, "ˌ"),
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub struct NumberedTone(u8);
+
+impl NumberedTone {
+    // `n` is a number from 0 to 9, 0 is no tone/unmarked tone and the rest are that number in superscript.
+    pub fn new(n: u8) -> Self {
+        match n {
+            0..=10 => NumberedTone(n),
+            _ => panic!("Invalid value, `n` has to be in 0..=9"),
+        }
+    }
+}
+
+impl Display for NumberedTone {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.0 {
+            0 if f.sign_plus() => write!(f, "."),
+            0 => Ok(()),
+            1 => write!(f, "¹"),
+            2 => write!(f, "²"),
+            3 => write!(f, "³"),
+            4 => write!(f, "⁴"),
+            5 => write!(f, "⁵"),
+            6 => write!(f, "⁶"),
+            7 => write!(f, "⁷"),
+            8 => write!(f, "⁸"),
+            9 => write!(f, "⁹"),
+            _ => unreachable!(),
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
-pub struct Syllable<C, V> {
-    stress: Stress,
+pub struct Syllable<C, V, S = Stress> {
+    stress: S,
     onset: Vec<C>,
     nucleus: V,
     coda: Vec<C>,
 }
 
-impl<C, V> Syllable<C, V> {
-    pub fn new(stress: Stress, onset: Vec<C>, nucleus: V, coda: Vec<C>) -> Self {
+impl<C, V, S> Syllable<C, V, S> {
+    pub fn new(stress: S, onset: Vec<C>, nucleus: V, coda: Vec<C>) -> Self {
         Syllable { stress, onset, nucleus, coda }
     }
 }
-impl Syllable<char, String> {
-    pub fn from(stress: Stress, onset: &str, nucleus: &str, coda: &str) -> Self {
+impl<S> Syllable<char, String, S> {
+    pub fn from(stress: S, onset: &str, nucleus: &str, coda: &str) -> Self {
         Syllable { stress, onset: onset.chars().collect(), nucleus: nucleus.to_owned(), coda: coda.chars().collect() }
     }
 }
 
-impl<C: Display, V: Display> Display for Syllable<C, V> {
+impl<C: Display, V: Display, S: Display> Display for Syllable<C, V, S> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self.stress {
-            Stress::None if f.sign_plus() => write!(f, ".")?,
-            Stress::None => (),
-            Stress::PrimaryStress => write!(f, "ˈ")?,
-            Stress::SecondaryStress => write!(f, "ˌ")?,
+        if f.sign_plus() {
+            write!(f, "{:+}", self.stress)?;
+        } else {
+            write!(f, "{}", self.stress)?;
         }
 
         for c in &self.onset {
@@ -52,9 +94,9 @@ impl<C: Display, V: Display> Display for Syllable<C, V> {
 }
 
 #[derive(Debug, Clone)]
-pub struct Word<C, V>(pub Vec<Syllable<C, V>>);
+pub struct Word<C, V, S = Stress>(pub Vec<Syllable<C, V, S>>);
 
-impl<C: Display, V: Display> Display for Word<C, V> {
+impl<C: Display, V: Display, S: Display> Display for Word<C, V, S> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut iter = self.0.iter();
 
